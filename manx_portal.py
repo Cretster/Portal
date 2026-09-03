424473a57434eda754fde571e9fb20add8d90e9a
Dr Pablo <cret123@gmail.com>
2026-09-03 23:14:36 +0100

    Comment out map interaction heading
    Comment out heading for map interaction instructions.

--- manx_portal.py
+++ manx_portal.py
@@ -396,7 +396,7 @@ def build_clickable_ph_map(center_lat=54.23, center_lon=-4.55, zoom=10,
 # --- FRONTEND ---
 st.set_page_config(page_title="Dr Pablo's Mushroom Magic!", page_icon="🍄", layout="wide")
 st.title("🍄 Dr Pablo's My Celium Magic!")
-st.heading("🍄 Click on the map where you want to check conditions")
+#st.heading("🍄 Click on the map where you want to check conditions")
 #st.heading("🍄 Or use a postcode for checking a more general area")
 
 if "map_click" not in st.session_state:
