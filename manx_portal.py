f7fada1d5e694355bcdfcc77bf61f004093a18d3
Dr Pablo <cret123@gmail.com>
2026-09-05 13:14:30 +0100

    Add usage instructions to the mushroom finder page
    Added usage instructions for the mushroom finder page.

--- manx_portal.py
+++ manx_portal.py
@@ -517,6 +517,7 @@ def growth_map_legend_html():
 st.set_page_config(page_title="Dr Pablo's Mushroom Magic!", page_icon="🍄", layout="wide")
 st.title("🍄 Dr Pablo's Mushroom Magic!")
 st.caption("Welcome to Dr Pablo's mushroom finder page.  Here at Dr Pablo industries we recognise that these tasty natural treats can be sometimes be a touch elusive. This page can help you check current/recent conditions likely to stimulate their growth, for any area you choose.")
+st.text("HOW TO USE:")
 with st.container(gap=1):
     st.text("1) Use the Zoom (+/-) buttons (for reliability compared to screen pinch), then Click on a location you're interested in on the map.")
     st.text("2) Then once you have clicked a spot, weather patterns will be retrieved and shown below the map, along with the soil pH")
